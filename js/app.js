ee511505ca44eb7465fe8e77bcb14dc648ceedc6
Hugo Oyola <hugo.ham@outlook.com>
2024-04-15 20:37:54 -0500

    ✨ Corregir errores y añadir funcionalidad en app.js

--- js/app.js
+++ js/app.js
@@ -1,34 +1,34 @@
 let cliente = {
-  mesa: '',
-  hora: '',
-  pedido: []
-}
+  mesa: "",
+  hora: "",
+  pedido: [],
+};
 
 const categorias = {
-  1: 'Comida',
-  2: 'Bebida',
-  3: 'Postre'
-}
+  1: "Comida",
+  2: "Bebida",
+  3: "Postre",
+};
 
-const btnGuardarCliente = document.querySelector('#guardar-cliente');
-btnGuardarCliente.addEventListener('click', guardarCliente);
+const btnGuardarCliente = document.querySelector("#guardar-cliente");
+btnGuardarCliente.addEventListener("click", guardarCliente);
 
 function guardarCliente() {
-  const mesa = document.querySelector('#mesa').value;
-  const hora = document.querySelector('#hora').value;
+  const mesa = document.querySelector("#mesa").value;
+  const hora = document.querySelector("#hora").value;
 
   // Revisar si campos están vacíos
-  const camposVacios = [ mesa, hora ].some( campo => campo === '' );
+  const camposVacios = [mesa, hora].some((campo) => campo === "");
 
-  if ( camposVacios ) {
+  if (camposVacios) {
     // Verificar si ya existe una alerta
-    const existeAlerta = document.querySelector('.invalid-feedback');
+    const existeAlerta = document.querySelector(".invalid-feedback");
 
-    if ( !existeAlerta ) {
-      const alerta = document.createElement('DIV');
-      alerta.classList.add('invalid-feedback', 'd-block', 'text-center');
-      alerta.textContent = 'Todos los campos son obligatorios';
-      document.querySelector('.modal-body form').appendChild(alerta);
+    if (!existeAlerta) {
+      const alerta = document.createElement("DIV");
+      alerta.classList.add("invalid-feedback", "d-block", "text-center");
+      alerta.textContent = "Todos los campos son obligatorios";
+      document.querySelector(".modal-body form").appendChild(alerta);
 
       setTimeout(() => {
         alerta.remove();
@@ -38,10 +38,10 @@ function guardarCliente() {
   }
 
   // Asignar datos del formulario al cliente.
-  cliente = { ...cliente, mesa, hora }
+  cliente = { ...cliente, mesa, hora };
 
   // Ocultar modal
-  const modalFormulario = document.querySelector('#formulario');
+  const modalFormulario = document.querySelector("#formulario");
   const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
   modalBootstrap.hide();
 
@@ -53,42 +53,54 @@ function guardarCliente() {
 }
 
 function mostrarSecciones() {
-  const seccionesOcultar = document.querySelectorAll('.d-none');
-  seccionesOcultar.forEach( seccion => seccion.classList.remove('d-none') );
+  const seccionesOcultar = document.querySelectorAll(".d-none");
+  seccionesOcultar.forEach((seccion) => seccion.classList.remove("d-none"));
 }
 
 function obtenerPlatillos() {
-  const url = 'http://localhost:4000/platillos';
+  const url = "http://localhost:4000/platillos";
 
   fetch(url)
-    .then( respuesta => respuesta.json())
-    .then( resultado => mostrarPlatillos(resultado))
-    .catch( error => console.log(error));
+    .then((respuesta) => respuesta.json())
+    .then((resultado) => mostrarPlatillos(resultado))
+    .catch((error) => console.log(error));
 }
 
 function mostrarPlatillos(platillos) {
-  const contenido = document.querySelector('#platillos .contenido')
+  const contenido = document.querySelector("#platillos .contenido");
 
-  platillos.forEach( platillo => {
-    const row = document.createElement('DIV');
-    row.classList.add('row', 'py-3', 'border-top');
+  platillos.forEach((platillo) => {
+    const row = document.createElement("DIV");
+    row.classList.add("row", "py-3", "border-top");
 
-    const nombre = document.createElement('DIV');
-    nombre.classList.add('col-4');
+    const nombre = document.createElement("DIV");
+    nombre.classList.add("col-4");
     nombre.textContent = platillo.nombre;
 
-    const precio = document.createElement('DIV');
-    precio.classList.add('col-3', 'fw-bold');
+    const precio = document.createElement("DIV");
+    precio.classList.add("col-3", "fw-bold");
     precio.textContent = `S/. ${platillo.precio}`;
 
-    const categoria = document.createElement('DIV');
-    categoria.classList.add('col-3');
+    const categoria = document.createElement("DIV");
+    categoria.classList.add("col-3");
     categoria.textContent = categorias[platillo.categoria];
+
+    const inputCantidad = document.createElement("INPUT");
+    inputCantidad.type = "number";
+    inputCantidad.min = 0;
+    inputCantidad.value = 0;
+    inputCantidad.id = `producto-${platillo.id}`;
+    inputCantidad.classList.add("form-control", "col-2");
+
+    const agregar = document.createElement("DIV");
+    agregar.classList.add("col-2");
+    agregar.appendChild(inputCantidad);
 
     row.appendChild(nombre);
     row.appendChild(precio);
     row.appendChild(categoria);
+    row.appendChild(agregar);
 
     contenido.appendChild(row);
-  })
+  });
 }
