24d93aa21381357afd70dd8ded64c8d6ee2337e9
Hugo Oyola <hugo.ham@outlook.com>
2024-04-15 22:22:13 -0500

    ✨ Añadir funcionalidad de mostrar resumen de pedido en app.js

--- js/app.js
+++ js/app.js
@@ -113,5 +113,86 @@ function mostrarPlatillos(platillos) {
 }
 
 function agregarPlatillo(producto) {
-  console.log(producto);
+  // Extraer el pedido actual
+  let { pedido } = cliente;
+
+  // Revisar que la cantidad sea mayor a 0
+  if (producto.cantidad > 0) {
+    // Comprueba si el producto ya está en el pedido
+    if (pedido.some((articulo) => articulo.id === producto.id)) {
+      // EL articulo ya existe, Actualizar la cantidad
+      const pedidoActualizado = pedido.map((articulo) => {
+        if (articulo.id === producto.id) {
+          articulo.cantidad = producto.cantidad;
+        }
+        return articulo;
+      });
+      // Se asigna el nuevo array a cliente.pedido
+      cliente.pedido = [...pedidoActualizado];
+    } else {
+      // El articulo no existe, lo agregamos al array de pedido
+      cliente.pedido = [...pedido, producto];
+    }
+  } else {
+    // Eliminar elementos cuando la cantidad es 0
+    const resultado = pedido.filter((articulo) => articulo.id !== producto.id);
+    cliente.pedido = [...resultado];
+  }
+
+  // Limpiar el codigo HTML previo
+  limpiarHTML()
+
+  // Mostrar el resumen
+  actualizarResumen()
+}
+
+function actualizarResumen(){
+  const contenido = document.querySelector("#resumen .contenido");
+
+  const resumen = document.createElement("DIV");
+  resumen.classList.add("col-md-6", "card","py-5", "px-3", "shadow");
+
+  // Informaciòn de la mesa
+  const mesa = document.createElement("P");
+  mesa.textContent = `Mesa: `;
+  mesa.classList.add("fw-bold");
+
+  const mesaSpan = document.createElement("SPAN");
+  mesaSpan.textContent = cliente.mesa;
+  mesa.classList.add("fw-normal");
+
+  // Información de la hora
+  const hora = document.createElement("P");
+  hora.textContent = `Hora: `;
+  hora.classList.add("fw-bold");
+
+  const horaSpan = document.createElement("SPAN");
+  horaSpan.textContent = cliente.hora;
+  horaSpan.classList.add("fw-normal");
+
+  // Agregar a los elementos padre
+  mesa.appendChild(mesaSpan);
+  hora.appendChild(horaSpan);
+
+  // Titulo de la sección
+  const heading = document.createElement("H3");
+  heading.textContent = "Platillos consumidos";
+  heading.classList.add("my-4", "text-center");
+
+  // Iterar sobre el array de pedido
+
+  // Agregar al contenido
+  resumen.appendChild(mesa);
+  resumen.appendChild(hora);
+  resumen.appendChild(heading);
+
+  contenido.appendChild(resumen);
+}
+
+function limpiarHTML(){
+  const contenido = document.querySelector("#resumen .contenido");
+
+  while(contenido.firstChild){
+    contenido.removeChild(contenido.firstChild);
+  }
 }
